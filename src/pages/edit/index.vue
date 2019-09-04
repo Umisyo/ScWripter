9726d156f1472a201f453f50af32cc9d1f20aeb9
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-04 16:11:41 +0900

    feat: ディレクトリ構成の変更

--- src/pages/edit/index.vue
+++ src/pages/edit/index.vue
@@ -1,19 +1,20 @@
 <template>
 <div>
-    <v-btn text>送信</v-btn>
+    <v-btn text @click="pushScripts">送信</v-btn>
     <div class="container">
+        <v-textarea v-model="acto_script.title"></v-textarea>
         <div class="textarea" v-if="!$device.isMobile">
             <v-textarea v-model="text" @input="format_text" counter auto-grow/>
         </div>
         <div class="mobile_textarea" v-else>
             <v-btn text @click="show_preview">Preview</v-btn>
             <div class="mobile_preview" v-if="isShowPreview">
-                <p v-for="(Paragraph, index) in text_array" :key="index"><br v-if="Paragraph === ''">{{ Paragraph }}</p>
+                <p v-for="(Paragraph, index) in acto_script.text_array" :key="index"><br v-if="Paragraph === ''">{{ Paragraph }}</p>
             </div>
             <v-textarea v-else v-model="text" @input="format_text" counter />
         </div>
         <div class="text_preview" v-if="!$device.isMobile">
-            <p v-for="(Paragraph, index) in text_array" :key="index"><br v-if="Paragraph === ''">{{ Paragraph }}</p>
+            <p v-for="(Paragraph, index) in acto_script.text_array" :key="index"><br v-if="Paragraph === ''">{{ Paragraph }}</p>
         </div>
     </div>
 </div>
@@ -27,8 +28,11 @@ export default {
         return {
             text: '',
             formatted_text: '',
-            text_array: [],
-            isShowPreview: false
+            isShowPreview: false,
+            acto_script: {
+                title: '',
+                text_array: []
+            }
         }
     },
     methods: {
@@ -38,39 +42,16 @@ export default {
         },
         async call_formatter() {
             const res = await wasm_text_formatter.then(wasm_text_formatter => wasm_text_formatter.text_parser(this.text))
-            this.text_array = res.split('\n')
+            this.acto_script.text_array = res.split('\n')
         },
         show_preview() {
             this.isShowPreview = !this.isShowPreview
+        },
+        pushScripts() {
+            console.log(this.acto_script)
         }
     }
 }
 </script>
 
-<style lang="scss" scoped>
-.container {
-    display: flex;
-    justify-content: space-around;
-    .textarea {
-        width: 50%;
-        padding: 5px;
-    }
-    .mobile_textarea {
-        width: 100%;
-    }
-    .text_preview {
-        flex-direction: column;
-        width: 50%;
-        min-height: 100vh;
-        height: 100%;
-        margin-top: 10px;
-        padding: 5px;
-        box-sizing: inherit;
-        border: solid 3px #000000;
-        p {
-            max-width: 100%;
-            word-break: break-all;
-        }
-    }
-}
-</style>
+<style lang="scss" scoped src="./style.scss" />
