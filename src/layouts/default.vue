84d875ed5df4a2b5ac3ae1931518faaae274c1ab
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-08 18:12:25 +0900

    feat: layoutの雛形を作成

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,31 @@
 <template>
-  <nuxt />
+  <div>
+    <header class="header">
+      <p class="header__logo">ScWripter</p>
+      <div class="header__user_info">
+        <v-btn v-if="!user" @click="googleLogin">login</v-btn>
+        <div v-else>
+          <nuxt-link to="/mypage">{{ user.name }}</nuxt-link>
+        </div>
+      </div>
+    </header>
+    <nuxt />
+  </div>
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  computed: {
+    user() {
+      return this.$store.state.auth.user;
+    }
+  },
+  methods: {
+    ...mapActions("auth", ["googleLogin"])
+  }
+};
+</script>
+
+<style lang="scss" src="./style.scss" />
